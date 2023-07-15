bbbdf21e3cefa3147a976a0696519cd299873a37
TheMindBreaker <A01229632@tec.mx>
2023-07-15 11:37:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import tkinter as tk
  
-tk._test()
+# creating window
+window = tk.Tk()
+ 
+# setting attribute
+window.attributes('-fullscreen', True)
+window.title("Geeks For Geeks")
+ 
+# creating text label to display on window screen
+label = tk.Label(window, text="Hello Tkinter!")
+label.pack()
+ 
+window.mainloop()
