4a6ac910b292ffa95ebe3802e4ff00884227e231
TheMindBreaker <A01229632@tec.mx>
2023-07-15 11:32:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
 import tkinter as tk
  
-# creating window
-window = tk.Tk()
- 
-# setting attribute
-window.attributes('-fullscreen', True)
-window.title("ACESS CONTROL V1.0.0")
- 
-# creating text label to display on window screen
-label = tk.Label(window, text="Hello Tkinter!")
-label.pack()
- 
-window.mainloop()
+tk._test()
