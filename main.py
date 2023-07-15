a0e4f1586bb3bd31bdb0af7599b130e68a615a94
TheMindBreaker <A01229632@tec.mx>
2023-07-15 12:07:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-import tkinter as tk
- 
-# creating window
-window = tk.Tk()
- 
-# setting attribute
-window.attributes('-fullscreen', True)
-window.title("Geeks For Geeks")
- 
-# creating text label to display on window screen
-label = tk.Label(window, text="Hello Tkinter!")
-label.pack()
- 
-window.mainloop()
+import customtkinter as tk
+
+class MyCheckboxFrame(tk.CTkFrame):
+    def __ini__(self):
+        tk.CTkLabel(self, text="Text1")
+
+class frame2(tk.CTkFrame):
+    def __ini__(self):
+        tk.CTkLabel(self, text="Text2")
+
+class App(tk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("my app")
+        self.attributes("-fullscreen", "True")
+        self.grid_columnconfigure((0, 1), weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.checkbox_frame_1 = MyCheckboxFrame(self)
+        self.checkbox_frame_1.grid(row=0, column=0, padx=5, pady=(10, 0), sticky="nsew")
+        self.checkbox_frame_2 = frame2(self)
+        self.checkbox_frame_2.grid(row=0, column=1, padx=(0, 5), pady=(10, 0), sticky="nsew")
+
+
+app = App()
+app.mainloop()
