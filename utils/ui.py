3459468adcbeec97504b17ae7945e0d9c5d556a4
CNSMX <A01229632@tec.mx>
2023-07-29 18:14:11 -0600

    Update ui.py

--- utils/ui.py
+++ utils/ui.py
@@ -1,11 +1,10 @@
 from wiegand import Wiegand
 import config
 
-GREEN_LED = Pin(config.LED_WIEGAND)
 
 def on_card(card_number, facility_code, cards_read):
 	print(card_number)
 	print(facility_code)
 	print(card_number)
 	
-Wiegand(config.WIEGAND[0], config.WIEGAND[1], on_card)
+Wiegand(pin0=config.WIEGAND[0], pin1=config.WIEGAND[1], callback=on_card)
