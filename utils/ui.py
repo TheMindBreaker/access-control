c904b807c27db42f42e50f77899c0d0de878af44
CNSMX <A01229632@tec.mx>
2023-07-29 18:46:32 -0600

    Update ui.py

--- utils/ui.py
+++ utils/ui.py
@@ -1,5 +1,5 @@
 from cns_wiegand import Wiegand
-
+import RPi.GPIO as GPIO
 
 WIEGAND_ZERO = 4
 WIEGAND_ONE = 17
@@ -8,5 +8,9 @@ def on_card(facility_code, card_code):
     print('Facility Code: ', facility_code)
     print('Card Code: ', card_code)
 
-wg = Wiegand(3, 4, on_card)
-wg.listen()
+try:
+    wg = Wiegand(3, 4, on_card)
+    # Other code
+finally:
+    GPIO.cleanup()
+
